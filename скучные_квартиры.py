901d30d4cb7c1db994d3dd31252084f8bb41de8f
HaizoAndCode <tihonovdima3307@gmail.com>
2023-10-31 14:00:14 +0300

    simplification

--- скучные_квартиры.py
+++ скучные_квартиры.py
@@ -1,5 +1,5 @@
 n, k = int(input()), int(input())
-if (k % 11 == 0 and k < 100) or k in [1, 2, 3, 4, 5, 6, 7, 8, 9] or k % 111 == 0:
+if (k % 11 == 0 and k < 100) or k % 111 == 0:
     print(k // 100 + k // 10 % 10 + k % 10)
 else:
     print(k)
